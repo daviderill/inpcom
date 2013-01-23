69fe2ba3ccc25e34ec81ff005d47e853bcbce7af
David Erill <daviderill79@gmail.com>
2013-01-23 11:42:33 +0100

    Version 1.7.2

---
+++ src/com/tecnicsassociats/gvsig/inpcom/Constants.java
@@ -0,0 +1,51 @@
+/*
+ * This file is part of INPcom
+ * Copyright (C) 2012  Tecnics Associats
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Author:
+ *   David Erill <daviderill79@gmail.com>
+ */
+
+package com.tecnicsassociats.gvsig.inpcom;
+
+public class Constants {
+
+	public static final String INI_FILE = "inp.properties";
+	public static final String DIR_SHP = "DIR_SHP";
+
+	public static final String CONFIG_PLUGIN = "com.tecnicsassociats.gvsig.inpcom";	    
+	public static final String FORMSPACKAGE = "com/tecnicsassociats/gvsig/inpcom/";
+	public static final String MAIN_FORM = FORMSPACKAGE + "FormExport.jfrm";
+
+	public static final String LBL_TITLE = "lbl_title";  //com.jeta.forms.components.label.JETALabel
+	public static final String LBL_DIR_OUT = "lbl_dir_out";  //com.jeta.forms.components.label.JETALabel
+	public static final String LBL_DIR_SHP = "lbl_dir_shp";  //com.jeta.forms.components.label.JETALabel
+	public static final String LBL_FILE_OUT = "lbl_file_out";  //com.jeta.forms.components.label.JETALabel
+	public static final String LBL_VERSION = "lbl_version";  //com.jeta.forms.components.label.JETALabel	
+	public static final String TXT_FILE_OUT = "txt_file_out";  //javax.swing.JFormattedTextField
+	public static final String TXT_VERSION = "txt_version";  //com.jeta.forms.components.label.JETALabel
+	public static final String BTN_CANCEL = "btn_cancel";  //javax.swing.JToggleButton
+	public static final String BTN_ACCEPT = "btn_accept";  //javax.swing.JToggleButton
+	public static final String BTN_HELP_TEMPLATE = "btn_help_template";  //javax.swing.JToggleButton
+	public static final String TXT_DIR_SHP = "txt_dir_shp";  //javax.swing.JFormattedTextField
+	public static final String TXT_DIR_OUT = "txt_dir_out";  //javax.swing.JFormattedTextField
+	public static final String BTN_FOLDER_SHP = "btn_folder_shp";  //javax.swing.JButton
+	public static final String BTN_FOLDER_OUT = "btn_folder_out";  //javax.swing.JButton
+	public static final String OPT_SWMM = "opt_swmm";  //javax.swing.JRadioButton
+	public static final String OPT_EPANET = "opt_epanet";  //javax.swing.JRadioButton
+	public static final String CHK_POLYGONS = "chk_polygons";  	
+
+}
---
+++ src/com/tecnicsassociats/gvsig/inpcom/MainWindow.java
@@ -0,0 +1,90 @@
+/*
+ * This file is part of INPcom
+ * Copyright (C) 2012  Tecnics Associats
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Author:
+ *   David Erill <daviderill79@gmail.com>
+ */
+
+package com.tecnicsassociats.gvsig.inpcom;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+
+import com.iver.andami.PluginServices;
+import com.iver.andami.ui.mdiManager.SingletonWindow;
+import com.iver.andami.ui.mdiManager.WindowInfo;
+import com.jeta.forms.components.panel.FormPanel;
+
+
+public class MainWindow extends JPanel implements SingletonWindow {
+
+	private static final long serialVersionUID = -4706517331314435778L;				
+	private FormPanel form;
+	private WindowController controller;
+
+	
+	public MainWindow(InpExtension model) {		
+		
+		setLayout(new BorderLayout());	    						
+		this.form = new FormPanel(Constants.MAIN_FORM);			
+		this.controller = new WindowController(model, this);				
+		
+		add(this.form, BorderLayout.CENTER);
+
+		form.getRadioButton(Constants.OPT_EPANET).addActionListener(controller);
+		form.getRadioButton(Constants.OPT_SWMM).addActionListener(controller);
+		form.getButton(Constants.BTN_FOLDER_SHP).addActionListener(controller);
+		form.getButton(Constants.BTN_FOLDER_OUT).addActionListener(controller);
+		form.getButton(Constants.BTN_ACCEPT).addActionListener(controller);	
+		form.getButton(Constants.BTN_CANCEL).addActionListener(controller);			
+		form.getButton(Constants.BTN_HELP_TEMPLATE).addActionListener(controller);			
+		
+		this.revalidate();
+		
+	}
+	
+
+	public WindowInfo getWindowInfo() {		
+		WindowInfo m_viewInfo = new WindowInfo(WindowInfo.RESIZABLE);
+    	m_viewInfo.setWidth(540);   // 480
+		m_viewInfo.setHeight(350);   // 300
+		m_viewInfo.setTitle(PluginServices.getText(this, "inp_descr"));		
+		return m_viewInfo;
+	}
+	
+	public void setWidth(int width) {
+		this.getWindowInfo().setWidth(width);
+	}
+
+	public Object getWindowProfile() {
+		return null;
+	}
+
+	public Object getWindowModel() {
+		return null;
+	}
+
+	public FormPanel getForm() {
+		return this.form;
+	}
+	
+	public WindowController getController() {
+		return controller;
+	}	
+
+}
